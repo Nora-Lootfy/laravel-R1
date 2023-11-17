1187d0fa1e2feef9130ed5add81a228d423f1223
Nora-Lootfy <96319423+Nora-Lootfy@users.noreply.github.com>
2023-11-17 02:57:35 +0200

    adding car controller

---
+++ app/Http/Controllers/CarController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CarController extends Controller
+{
+    public function add_car() {
+        return view('addCar');
+    }
+
+    public function added(Request $request) {
+        $data = $request->all();
+        $data["published"] = isset($data["published"])? "Yes": "No";
+
+        return view('carAdded', $data);
+    }
+}
