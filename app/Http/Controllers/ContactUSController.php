a0f37590cc53ca95b9067c1813057161783394a2
Nourhan Abdelaziz <Nourhan33abdelaziz@gmail.com>
2024-01-13 19:13:13 +0200

    task16, dealing with laravel queues

--- app/Http/Controllers/ContactUSController.php
+++ app/Http/Controllers/ContactUSController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\ContactUS;
+use App\Jobs\SendEmailJob;
 
 
 class ContactUSController extends Controller
@@ -23,6 +24,7 @@ class ContactUSController extends Controller
         ]);
 
         Mail::to(env('MAIL_FROM_ADDRESS'))->send(new ContactUS($data));
+        dispatch(new SendEmailJob($data));
         return 'message sent, thanks';
     }
 
